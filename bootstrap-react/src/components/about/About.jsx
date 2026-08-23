350268d7c6058fa879d3bb53798337108a6398d5
007codename <juri.malaj@gmail.com>
2026-08-23 15:04:03 +0200

    header: nav bar add | home: jumbotron add

---
+++ bootstrap-react/src/components/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./about.css";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
