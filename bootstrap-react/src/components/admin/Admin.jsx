9b46faa7c2379166b5d7e907d0b8d3588f40e258
007codename <juri.malaj@gmail.com>
2026-09-02 14:15:12 +0200

    fix: link admin css

--- bootstrap-react/src/components/admin/Admin.jsx
+++ bootstrap-react/src/components/admin/Admin.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./admin.css";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import ListGroup from "react-bootstrap/ListGroup";
